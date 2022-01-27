ae6da8783a50086f3205db87e7f12e10b3735edc
gileshd <gileshd@googlemail.com>
2022-01-27 15:31:17 +0000

    Fix OUT_FILE name.

--- plots/joint_inference_joint_boundary.py
+++ plots/joint_inference_joint_boundary.py
@@ -9,7 +9,7 @@ from plotting_tools import plot_joint_with_marginals
 
 plt.style.use("thesis")
 
-OUT_FILE = "SDE_figures/joint_inference_joint_boundaries.pdf"
+OUT_FILE = "SDE_figures/joint_inference_joint_boundary.pdf"
 
 lims = (-5,5)
 
