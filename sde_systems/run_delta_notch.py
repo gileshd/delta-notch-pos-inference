e246e85bb2cb29bee21bfe44ea9794742b4935c1
gileshd <gileshd@googlemail.com>
2022-02-02 19:15:43 +0000

    Add drop_nans to run_sde output.

--- sde_systems/run_delta_notch.py
+++ sde_systems/run_delta_notch.py
@@ -6,29 +6,36 @@ import jax
 from sde_systems.jax_delta_notch import DN_sdeint_ito
 from sde_systems.delta_notch_tools import (
     write_sde_results,
-    load_sde_results,
     calculate_y0_std,
-    sample_y0_DN_mixed,
+    sample_y0_DN,
+    drop_nans_DN
 )
 
 noise_scale = 0
 args = (0.01, 100, 4, 4, noise_scale)
 
-def run_delta_notch_sde(args, n_samples):
+
+def run_delta_notch_sde(args, n_samples, z=0, dt=0.01, drop_nans=True):
     """ Simulate Delta-Notch system with `args`, `n_samples` times. """
     t0, t1 = 0, 6
     nsteps = 100
     ts = jnp.linspace(t0, t1, nsteps)
 
-    μ1, μ2 = 0.4,0.6
-    std = calculate_y0_std(μ1,μ2)
-    cov = np.diag([1,1]) * std**2
-    z_batch_size = int(n_samples/2)
+    μ1, μ2 = 0.4, 0.6
+    std = calculate_y0_std(μ1, μ2)
+    cov = np.diag([1, 1]) * std ** 2
 
-    y0 = sample_y0_DN_mixed(μ1, μ2, cov, z_batch_size)
+    μ = np.array([μ1, μ2])
+    z_idx = [[0, 1], [1, 0]][z]
+    y0 = sample_y0_DN(*μ[z_idx], cov, n_samples)
     rng = random.PRNGKey(0)
     rngs = random.split(rng, n_samples)
 
-    DN_sdeint = jit(vmap(lambda y0, rng: DN_sdeint_ito(y0, ts, rng, args, dt=0.01)))
+    DN_sdeint = jit(vmap(lambda y0, rng: DN_sdeint_ito(y0, ts, rng, args, dt=dt)))
 
-    return DN_sdeint(y0,rngs)
+    ys = DN_sdeint(y0, rngs)
+    if drop_nans:
+        ys = drop_nans_DN(ys)
+
+    return ys 
+
