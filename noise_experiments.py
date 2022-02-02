0fd66eb4b8b6c35413168e46c9f4c06b8e63aacd
gileshd <gileshd@googlemail.com>
2022-02-02 23:49:07 +0000

    Run with 101 noise values.

--- noise_experiments.py
+++ noise_experiments.py
@@ -3,9 +3,9 @@ import jax; jax.config.update('jax_platform_name', 'cpu')
 from sde_systems.delta_notch_tools import write_sde_results
 from sde_systems.run_delta_notch import run_delta_notch_sde
 
-RESULT_DIR = "results/noise_experiments"
+RESULT_DIR = "results/noise_experiments/delta_delta_t0t1_2"
 
-noise_range = np.linspace(0,0.25,11)
+noise_range = np.linspace(0,0.25,101)
 n_samples = 100000
 
 for noise_scale in noise_range:
@@ -14,9 +14,11 @@ for noise_scale in noise_range:
 
     print(f"Running noise={noise_scale:.4f}...")
     ys = run_delta_notch_sde(args,n_samples)
+
+    ys_write = ys[:,[0,-1],:2]
     
-    noise_str = "{:0>3d}".format(int(noise_scale*1000))
+    noise_str = "{:0>4d}".format(int(noise_scale*10000))
     BASE_RESULTS_STR = f"{RESULT_DIR}/DN_traj_noise-{noise_str}_{{}}.txt"
 
     print(f"Writing results, noise={noise_scale:.4f}...")
-    write_sde_results(ys,BASE_RESULTS_STR)
+    write_sde_results(ys_write,BASE_RESULTS_STR)
