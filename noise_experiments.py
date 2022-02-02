758f8b3cc26c76c441e45bb2a03373c3bab64429
gileshd <gileshd@googlemail.com>
2022-02-02 19:23:47 +0000

    Format float in print statements.

--- noise_experiments.py
+++ noise_experiments.py
@@ -12,11 +12,11 @@ for noise_scale in noise_range:
     np.random.seed(123)
     args = (0.01, 100, 4, 4, noise_scale)
 
-    print(f"Running noise={noise_scale}...")
+    print(f"Running noise={noise_scale:.4f}...")
     ys = run_delta_notch_sde(args,n_samples)
     
     noise_str = "{:0>3d}".format(int(noise_scale*1000))
     BASE_RESULTS_STR = f"{RESULT_DIR}/DN_traj_noise-{noise_str}_{{}}.txt"
 
-    print(f"Writing results, noise={noise_scale}...")
+    print(f"Writing results, noise={noise_scale:.4f}...")
     write_sde_results(ys,BASE_RESULTS_STR)
