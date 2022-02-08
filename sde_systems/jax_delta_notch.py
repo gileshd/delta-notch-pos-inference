82c937d0630ad30b8422d0da0d591382c8b6b81d
gileshd <gileshd@googlemail.com>
2022-02-08 16:00:36 +0000

    Remove default args.

--- sde_systems/jax_delta_notch.py
+++ sde_systems/jax_delta_notch.py
@@ -2,13 +2,15 @@ from jax import numpy as jnp
 from jax import jit, vmap
 from jaxsde.jaxsde import sdeint_ito
 
-noise_scale = 0.05
-args = (0.01,100,4,4, noise_scale)
+# noise_scale = 0.05
+# args = (0.01, 100., 4., 4., noise_scale)
+
 
 def f(y, t, args):
     prod = production(y, args)
     dec = decay(y)
     return prod - dec
+
 
 def g(y, t, args):
     noise_scale = args[-1]
@@ -16,21 +18,25 @@ def g(y, t, args):
     dec = decay(y)
     return jnp.sqrt(prod + dec) * noise_scale
 
+
 def production(y, args):
     a, b, h, k, _ = args
-    delta = delta_f(y[2:],b,h)
-    notch = notch_f(jnp.flip(y[:2]),a,k)
-    return jnp.hstack((delta,notch))
+    delta = delta_f(y[2:], b, h)
+    notch = notch_f(jnp.flip(y[:2]), a, k)
+    return jnp.hstack((delta, notch))
 
-def delta_f(y,b,h):
+
+def delta_f(y, b, h):
     return 1 / (1 + b * y ** h)
 
-def notch_f(y,a,k):
+
+def notch_f(y, a, k):
     return y ** k / (a + y ** k)
+
 
 def decay(y):
     return y
 
-def DN_sdeint_ito(y0,ts,rng,args,dt=0.001):
-    return sdeint_ito(f,g,y0,ts,rng,args,dt)
 
+def DN_sdeint_ito(y0, ts, rng, args, dt=0.001):
+    return sdeint_ito(f, g, y0, ts, rng, args, dt)
