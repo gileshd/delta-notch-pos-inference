ef76429f56de546f33f9e59b695acfeee9ada218
gileshd <gileshd@googlemail.com>
2022-02-09 17:19:40 +0000

    Use fixed grid for performance estimates.

--- sde_systems/jax_delta_notch_tools.py
+++ sde_systems/jax_delta_notch_tools.py
@@ -8,7 +8,7 @@ from sde_systems.jax_delta_notch import DN_sdeint_ito, DN_sdeint_ito_fixed_grid
 
 t0 = 0
 t1 = 6
-nsteps = 100
+nsteps = 100 
 ts = jnp.linspace(t0, t1, nsteps)
 ts_fg = jnp.linspace(t0, t1*2, nsteps*2)
 
@@ -26,7 +26,8 @@ DN_sdeint_fg = jit(vmap(lambda y0, key, args: DN_sdeint_ito_fixed_grid(y0,ts_fg,
 @partial(jit,static_argnums=1)
 def sample_y0_eq_jit(key,n_samples=1000):
     μ = jnp.array([0.5,0.5])
-    cov = jnp.diag(jnp.ones(2)) * 0.05**2
+    cov = jnp.array([[1,0.5],
+                 [0.5,1]]) * 0.03**2
     y0_delta = random.multivariate_normal(key,μ,cov,shape=(n_samples,))
     y0_notch = jnp.ones((n_samples,2)) * 0.5
     return jnp.hstack((y0_delta,y0_notch))
@@ -58,18 +59,18 @@ def sample_args_perf(args,key,y0):
     return mean_perf_y0y1(ys)
 
 @jit
-def args_perf_single(args,key,y0):
+def args_perf_single(args,key):
     """ Sample performance of `args`."""
     y0 = sample_y0_eq_single(key)
-    ys = DN_sdeint_single(y0,key,args)
-    y0, y1 = ys[0,:2], y1[-1,:2]
+    ys = DN_sdeint_fg_single(y0,key,args)
+    y0, y1 = ys[0,:2], ys[-1,:2]
     return compare_y0y1(y0,y1)
 
 @jit
-def a_perf_single(a,key,y0):
+def a_perf_single(a,key):
     """ Sample performance of parameter `a`."""
     args = (a, 100.,4.,4.,0.05)
-    return args_perf_single(args,key,y0)
+    return args_perf_single(args,key)
 
 @jit
 def mean_perf_y0y1(ys):
