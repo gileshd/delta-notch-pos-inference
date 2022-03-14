a7b8f6274ed655b2ec7be36187681aa0f7cb3946
gileshd <gileshd@googlemail.com>
2022-03-14 16:05:40 +0000

    Update to show two values of beta.

--- plots/sde_opt/ab_optimality.py
+++ plots/sde_opt/ab_optimality.py
@@ -5,7 +5,6 @@ plt.style.use("thesis")
 
 OUT_FILE = "../SDE_figures/ab_optimality.pdf"
 
-
 ### LOAD DATA ###
 RESULTS_DIR = "../../ab_performance_results/param_points_100"
 N = 20
@@ -16,23 +15,25 @@ for i in range(N):
 
 P = P_array.mean(-1)
 
-### MAKE PLOT ###
-β = 15.
-O = np.exp(P*β)
-
 N_param_samples = 100
 a_array = np.logspace(-5,1, N_param_samples)
 b_array = np.logspace(-1,5, N_param_samples)
 
 A, B = np.meshgrid(a_array,b_array)
 
-fig, ax = plt.subplots(figsize=(8,8))
+fig, axs = plt.subplots(1,2,figsize=(8,4))
 
-ax.pcolormesh(A,B,O,shading="gouraud");
-ax.set_xscale("log")
-ax.set_yscale("log")
+beta_array = [1.,30.]
 
-ax.set_xlabel("a",fontsize=20)
-ax.set_ylabel("b",fontsize=20);
+#ax.pcolormesh(A,B,O,shading="gouraud");
+for β,ax in zip(beta_array,axs):
+    O = np.exp(P*β)
+    ax.pcolormesh(A,B,O,shading="gouraud");
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_xlabel("a",fontsize=15)
+    ax.set_ylabel("b",fontsize=15);
+    ax.text(0.1, 0.1,r"$\beta = {}$".format(β),color="w",transform=ax.transAxes)
 
+plt.tight_layout()
 plt.savefig(OUT_FILE,bbox_inches="tight")
