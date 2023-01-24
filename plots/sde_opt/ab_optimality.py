4aea2920c6737e2e7e32c04ce98fccd1dd3909f9
gileshd <gileshd@googlemail.com>
2023-01-24 18:36:29 +0800

    Add colorbar.

--- plots/sde_opt/ab_optimality.py
+++ plots/sde_opt/ab_optimality.py
@@ -9,6 +9,8 @@ OUT_FILE = "../SDE_figures/ab_optimality.pdf"
 P_RESULTS_DIR = "../../ab_performance_results/param_points_100/d1_thresh_0-5"
 P_FILE = "P_thresh_np100_10e3"
 
+normalise = lambda x: (x-x.min()) / (x.max() - x.min())
+
 N = 20
 P_array = np.zeros((100,100,N))
 for i in range(N):
@@ -21,19 +23,22 @@ b_array = np.logspace(-1,5, N_param_samples)
 
 A, B = np.meshgrid(a_array,b_array)
 
-fig, axs = plt.subplots(1,2,figsize=(8,4))
+fig, axs = plt.subplots(1,2,figsize=(10,4), constrained_layout=True)
+
 
 beta_array = [1.,10.]
 
-#ax.pcolormesh(A,B,O,shading="gouraud");
 for β,ax in zip(beta_array,axs):
     O = np.exp(P*β)
-    ax.pcolormesh(A,B,O,shading="gouraud");
+    O = normalise(O)
+    cmesh = ax.pcolormesh(A,B,O, shading="gouraud")
     ax.set_xscale("log")
     ax.set_yscale("log")
     ax.set_xlabel("a",fontsize=15)
     ax.set_ylabel("b",fontsize=15);
     ax.text(0.07, 0.07, r"$\beta = {}$".format(β),color="w",transform=ax.transAxes)
 
-plt.tight_layout()
+cbar = fig.colorbar(cmesh,ax=axs,fraction=0.2,aspect=40)
+cbar.set_label("Optimality (normalised)", rotation=270, labelpad=20, fontsize=12)
+
 plt.savefig(OUT_FILE,bbox_inches="tight")
